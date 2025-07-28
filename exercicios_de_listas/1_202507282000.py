d6125170e604d3ecac0715d5bcb913fa06669c40
Marcos Assuncao <dc.massuncao@gmail.com>
2025-07-28 20:15:14 -0300

    1_new_file

---
+++ exercicios_de_listas/1_202507282000.py
@@ -0,0 +1,39 @@
+# Faça um programa que preencha uma lista com 10 numeros, calcule e mostre os numeros primos e suas respectivas posições na lista.
+
+# Função para verificar se um número é primo
+import random
+
+
+def eh_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True 
+
+# Inicio do programa
+# A variável numeros inicia uma lista vazia para armazenar os números
+
+numeros = []    
+# Preenche a lista com 10 números inteiros fornecidos pelo usuário
+for i in range(10):
+    num = random.randint(1, 100)
+    numeros.append(num) 
+
+# Lista para armazenar os números primos e suas posições
+primos = []
+# Verifica cada número na lista e armazena os primos e suas posições    
+for i, num in enumerate(numeros):
+    if eh_primo(num):
+        primos.append((num, i))  # Armazena o número primo e sua posição    
+# Exibe os números primos e suas posições
+if primos:
+    
+    for num, pos in primos:
+        print(f"Número: {num}, Posição: {pos}")
+else:
+    print("Nenhum número primo encontrado na lista.")   
+    
+    
+# Fim do programa
