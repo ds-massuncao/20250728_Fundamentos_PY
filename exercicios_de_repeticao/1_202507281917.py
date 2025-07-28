5dfe2033810b50d10f601aa9b13f1b6613ba302a
Marcos Assuncao <dc.massuncao@gmail.com>
2025-07-28 19:56:57 -0300

    1_edited_file

--- exercicios_de_repeticao/1_202507281917.py
+++ exercicios_de_repeticao/1_202507281917.py
@@ -6,20 +6,23 @@
 # Variável n recebe um valor inteiro e positivo do usuário
 # A variável s inicia com 1, que é o primeiro termo da série    
 # A variável factorial inicia com 1, que é o fatorial de 0 (0! = 1)
+
 n = int(input("Digite um valor inteiro e positivo: "))
 s = 1.0
 factorial = 1
 
 # Verifica se o valor é positivo
+
 while n <0:
     n = int(input("Valor inválido. Digite um valor inteiro e positivo: "))
     
 
 # Calcula o valor de S
 # A variável s inicia com 1, que é o primeiro termo da série
+
 for i in range(1, n + 1):
-    factorial *= i  # Calcula o fatorial de i
-    s += 1 / factorial  # Adiciona o termo 1/i! à soma  
+    factorial *= i  
+    s += 1 / factorial 
     
 # Exibe o resultado com 6 casas decimais 
 print(f"O valor de S é: {s:.6f}")  
